58d4fca1263a06dc6b9a5085adceb84d01934d84
Robert Crump <119753396+rwrcrmp@users.noreply.github.com>
2023-10-10 08:56:05 -0500

    new blog commit 1

--- R/random_walks.R
+++ R/random_walks.R
@@ -6,16 +6,32 @@ lapply(libs, library, character.only = T)
 rm(libs)
 
 # load datasets
-atx_hex <- st_read("Desktop/IH_35_study/data/created_files/atx_hex/atx_hex.shp")
+atx_hex <- st_read("data/created_files/atx_hex/atx_hex.shp")
 
-source("Desktop/IH_35_study/R/multi_path.R")
+source("R/multi_path.R")
 
 sim_trial <- multi_path(atx_hex, 10, 10)
 
 # test plot
 atx_hex |> 
-  filter(FID %in% c(sim_trial[[2]])) |>
+  filter(FID %in% sim_trial[[5]]) |>
   ggplot() + 
   geom_sf() +
   geom_sf_label(aes(label = FID)) +
+  theme_void()
+
+atx_walks <- atx_hex |>
+  left_join(
+    sim_trial |>
+      as.data.frame() |>
+      pivot_longer(everything(),
+                   names_to = "walk",
+                   values_to = "FID")
+  )
+
+# test plot
+atx_walks |>
+  filter(walk == "walk_5") |>
+  ggplot() +
+  geom_sf(aes(fill = walk)) +
   theme_void()
